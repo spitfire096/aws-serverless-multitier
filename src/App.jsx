31025dfe2b84ff4a882e99c55f1e424bf6fede7c
johnbobelisa <johnelisaaa@gmail.com>
2025-01-13 16:41:29 +1100

    added more css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 // App.jsx
 import React from 'react'
 import Navbar from './components/navbar/navbar'
+import ProductListing from './components/products/ProductListing'
+
+import './App.css'
 
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <ProductListing />
     </div>
   )
 }
